f0b14088737b83730a7dfe0f092fa7f6cdfee749
dattran <ceilingdat@gmail.com>
2023-02-12 09:06:58 +0700

    fix resnet

--- app/core/network_models/model_ResNet.py
+++ app/core/network_models/model_ResNet.py
@@ -9,10 +9,10 @@ from keras.datasets import cifar10
 from keras.layers import Add, GlobalAveragePooling2D,Dense, Flatten, Conv2D, Lambda,Input , BatchNormalization, Activation
 from keras.optimizers import schedules, SGD
 from keras.callbacks import TensorBoard, ModelCheckpoint
-from app.core.settings import get_settings
+from core import setting
 
 
-setting = get_settings()
+setting = setting.get_settings()
 
 def residual_block(x, number_of_filters, match_filter_size=False):
 
