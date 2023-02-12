8ba42a00057b8b7cc801d13d9741e7117455d699
dattran <ceilingdat@gmail.com>
2023-02-12 14:42:33 +0700

    first api

---
+++ app/api/fer.py
@@ -0,0 +1,36 @@
+import os 
+import pathlib
+from fastapi import FastAPI
+from fastapi import FastAPI
+import io
+import sys
+cwd = os.getcwd()
+
+def get_root_project(cwd):
+    user_path = ''
+    path_list = cwd.split('/')
+    for i in path_list:
+        if str(i) == 'app':
+            return user_path
+        user_path = str(user_path) +  str(i) + '/'
+    return False
+
+user_path = get_root_project(cwd)
+
+
+sys.path.append(user_path)
+
+from app.core.settings import get_setting
+
+
+
+app = FastAPI()
+
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent
+
+
+
+@app.get('/')
+def first_api_test():
+    return {'hello': 'world'}
