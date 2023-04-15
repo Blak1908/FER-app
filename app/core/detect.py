02a7d10773b3fbb4671b182ce2247a9c6f2899d7
Blak1908 <ceilingdat@gmail.com>
2023-04-15 16:29:08 +0700

    update detect

--- app/core/detect.py
+++ app/core/detect.py
@@ -7,160 +7,64 @@ import os
 import torch 
 from torchvision import transforms
 
-from emotic import Emotic
-from app.core.utils.inference import infer
-from app.core.utils.yolo_utils import prepare_yolo, rescale_boxes, non_max_suppression
+from app.core.modules.emotic.utils.inference import infer
+from app.core.modules.emotic.utils.yolo_utils import prepare_yolo, rescale_boxes, non_max_suppression
+from app.core.settings import get_settings
+from app.core.modules import emotic
+
+settings = get_settings()
+
+modes = settings.MODES
+result_path = settings.MODEL_PATH
+
+if not os.path.exists(result_path):
+  os.system(f"mkdir {result_path}")
 
 
+def detect(context_norm, body_norm, ind2cat, ind2vad, args):
 
-
-
-def get_bbox(yolo_model, device, image_context, yolo_image_size=416, conf_thresh=0.8, nms_thresh=0.4):
-
-  test_transform = transforms.Compose([transforms.ToPILImage(),transforms.ToTensor()])
-  image_yolo = test_transform(cv2.resize(image_context, (416, 416))).unsqueeze(0).to(device)
-
-  with torch.no_grad():
-    detections = yolo_model(image_yolo)
-    nms_det  = non_max_suppression(detections, conf_thresh, nms_thresh)[0]
-    det = rescale_boxes(nms_det, yolo_image_size, (image_context.shape[:2]))
-  
-  bboxes = []
-  for x1, y1, x2, y2, _, _, cls_pred in det:
-    if cls_pred == 0:  # checking if predicted_class = persons. 
-      x1 = int(min(image_context.shape[1], max(0, x1)))
-      x2 = int(min(image_context.shape[1], max(x1, x2)))
-      y1 = int(min(image_context.shape[0], max(15, y1)))
-      y2 = int(min(image_context.shape[0], max(y1, y2)))
-      bboxes.append([x1, y1, x2, y2])
-  return np.array(bboxes)
-
-
-def yolo_infer(images_list, result_path, model_path, context_norm, body_norm, ind2cat, ind2vad, args):
-  ''' Infer on a list of images defined in images_list text file to obtain bounding boxes of persons in the images using yolo model.
-  :param images_list: Text file specifying the images to conduct inference. A row in the file is Path_of_image. 
-  :param result_path: Directory path to save the results (images with the predicted emotion categories and continuous emotion dimesnions).
-  :param model_path: Directory path to load models and val_thresholds to perform inference.
-  :param context_norm: List containing mean and std values for context images. 
-  :param body_norm: List containing mean and std values for body images. 
-  :param ind2cat: Dictionary converting integer index to categorical emotion. 
-  :param ind2vad: Dictionary converting integer index to continuous emotion dimension (Valence, Arousal and Dominance).
-  :param args: Runtime arguments.
-  '''
-  device = torch.device("cuda:%s" %(str(args.gpu)) if torch.cuda.is_available() else "cpu")
-  yolo = prepare_yolo(model_path)
-  yolo = yolo.to(device)
-  yolo.eval()
-
-
-  print("Start loading models...")
-  thresholds = torch.FloatTensor(np.load(os.path.join(result_path, 'val_thresholds.npy'))).to(device) 
-  model_context = torch.load(os.path.join(model_path,'model_context1.pth')).to(device)
-  model_body = torch.load(os.path.join(model_path,'model_body1.pth')).to(device)
-  emotic_model = torch.load(os.path.join(model_path,'model_emotic1.pth')).to(device)
-  
-  models = [model_context, model_body, emotic_model]
-
-  with open(images_list, 'r') as f:
-    lines = f.readlines()
-  
-  for idx, line in enumerate(lines):
-    image_context_path = line.split('\n')[0].split(' ')[0]
-    image_context = cv2.cvtColor(cv2.imread(image_context_path), cv2.COLOR_BGR2RGB)
-    try:
-      bbox_yolo = get_bbox(yolo, device, image_context)
-      for pred_bbox in bbox_yolo:
-        pred_cat, pred_cont = infer(context_norm, body_norm, ind2cat, ind2vad, device, thresholds, models, image_context=image_context, bbox=pred_bbox, to_print=False)
-        write_text_vad = list()
-        for continuous in pred_cont:
-          write_text_vad.append(str('%.1f' %(continuous)))
-        write_text_vad = 'vad ' + ' '.join(write_text_vad) 
-        image_context = cv2.rectangle(image_context, (pred_bbox[0], pred_bbox[1]),(pred_bbox[2] , pred_bbox[3]), (255, 0, 0), 3)
-        cv2.putText(image_context, write_text_vad, (pred_bbox[0], pred_bbox[1] - 5), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
-        for i, emotion in enumerate(pred_cat):
-          cv2.putText(image_context, emotion, (pred_bbox[0], pred_bbox[1] + (i+1)*12), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
-    except Exception as e:
-      print ('Exception for image ',image_context_path)
-      print (e)
-    cv2.imwrite(os.path.join(result_path, 'img_%r.jpg' %(idx)), cv2.cvtColor(image_context, cv2.COLOR_RGB2BGR))
-    print ('completed inference for image %d'  %(idx))
-
-
-
-def detect(result_path, model_path, context_norm, body_norm, ind2cat, ind2vad, args):
-
-  device = torch.device("cuda:%s" %(str(args.gpu)) if torch.cuda.is_available() else "cpu")
-  yolo = prepare_yolo(model_path)
-  yolo = yolo.to(device)
-  yolo.eval()
-
-  thresholds = torch.FloatTensor(np.load(os.path.join(result_path, 'val_thresholds.npy'))).to(device) 
-  model_context = torch.load(os.path.join(model_path,'model_context1.pth')).to(device)
-  model_body = torch.load(os.path.join(model_path,'model_body1.pth')).to(device)
-  emotic_model = torch.load(os.path.join(model_path,'model_emotic1.pth')).to(device)
-
-  model_context.eval()
-  model_body.eval()
-  emotic_model.eval()
-  models = [model_context, model_body, emotic_model]
-
-
-  # Capture frame from camera
-  cv2.ocl.setUseOpenCL(False)
-  cap = cv2.VideoCapture(int(args.source))
+  camera_mode, video_mode, image_mode = False, False, False 
+  if args.source == "0":
+    camera_mode = True
+    # Capture frame from camera
+    cv2.ocl.setUseOpenCL(False)
+    cap = cv2.VideoCapture(int(args.source))
+    
+  elif str(args.source).endswith(".mp4"):
+    video_mode = True
+    cap = cv2.VideoCapture(args.source)
+  else:
+    image_mode = True
+    base_name = str(args.source).split('/')[-1]
+    base_name = base_name.split('.')[0]
+    img = cv2.imread(args.source)
 
   while True:
-    _, frame = cap.read()
+    ret, frame = cap.read()
     image_context = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    try: 
-      bbox_yolo = get_bbox(yolo, device, image_context)
-      for pred_idx, pred_bbox in enumerate(bbox_yolo):
-        pred_cat, pred_cont = infer(context_norm, body_norm, ind2cat, ind2vad, device, thresholds, models, image_context=image_context, bbox=pred_bbox, to_print=False)
-        write_text_vad = list()
-        for continuous in pred_cont:
-          write_text_vad.append(str('%.1f' %(continuous)))
-        write_text_vad = 'vad ' + ' '.join(write_text_vad) 
+    if not ret:
+      print("End process! ")
+      break
+    
+    pred = emotic.emotic_model.frame_predict(context_norm ,body_norm, ind2cat, ind2vad, image_context)
 
-        # Draw a retangle into frame
-        image_context = cv2.rectangle(image_context, (pred_bbox[0], pred_bbox[1]),(pred_bbox[2] , pred_bbox[3]), (255, 0, 0), 3)
+    preds = []
+    if camera_mode:                                         
+      # Show real-time capture                                         )
+      cv2.imshow('Emotion Detector',pred)
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    elif video_mode:
+      preds.append(pred)
 
-        # Write predict categories into frame
-        cv2.putText(image_context, write_text_vad, (pred_bbox[0], pred_bbox[1] - 5), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
-        for i, emotion in enumerate(pred_cat):
-          cv2.putText(image_context, emotion, (pred_bbox[0], pred_bbox[1] + (i+1)*12), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
-        
-        # Show on the frames have predicted
-        cv2.imshow('Emotion Detector',image_context)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-          break  
-    except Exception:
-      pass
-
-  
-
-
-def check_paths(args):
-  ''' Check (create if they don't exist) experiment directories.
-  :param args: Runtime arguments as passed by the user.
-  :return: result_dir_path, model_dir_path.
-  '''
-  if args.inference_file is not None: 
-    if not os.path.exists(args.inference_file):
-      raise ValueError('inference file does not exist. Please pass a valid inference file')
-
-  model_path = os.path.join(args.experiment_path, args.model_dir)
-  if not os.path.exists(model_path):
-    raise ValueError('model path %s does not exist. Please pass a valid model_path' %(model_path))
-  result_path = os.path.join(args.experiment_path, args.result_dir)
-  if not os.path.exists(result_path):
-    os.makedirs(result_path)
-  return result_path, model_path
+    else:
+      cv2.imwrite(f"{result_path}/{base_name}.jpg", pred)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=0, help='gpu id')
-    parser.add_argument('--experiment_path', type=str, required=True, help='Path of experiment files (results, models, logs)')
+    parser.add_argument('--experiment_path', type=str, help='Path of experiment files (results, models, logs)')
     parser.add_argument('--model_dir', type=str, default='models', help='Folder to access the models')
     parser.add_argument('--result_dir', type=str, default='results', help='Path to save the results')
     parser.add_argument('--inference_file', type=str, help='Text file containing image context paths and bounding box')
@@ -176,8 +80,6 @@ def parse_args():
 
 if __name__=='__main__':
   args = parse_args()
-
-  result_path, model_path = check_paths(args)
 
   cat = ['Affection', 'Anger', 'Annoyance', 'Anticipation', 'Aversion', 'Confidence', 'Disapproval', 'Disconnection', \
           'Disquietment', 'Doubt/Confusion', 'Embarrassment', 'Engagement', 'Esteem', 'Excitement', 'Fatigue', 'Fear','Happiness', \
@@ -201,10 +103,10 @@ if __name__=='__main__':
   body_norm = [body_mean, body_std]
   if args.source is not None:
     print (f'inference over source: {args.source}')
-    detect(result_path, model_path, context_norm, body_norm, ind2cat, ind2vad, args)
+    detect(context_norm, body_norm, ind2cat, ind2vad, args)
   else:
     print('source input invalid !!')
 
 
 
-# python app/core/utils/detect.py --source 0 --experiment_path /home/dattran/Documents/dattran/FER-app/weights/model_saved
+# python app/core/detect.py --source 0 --experiment_path /home/dattran/Documents/dattran/FER-app/weights/model_saved
