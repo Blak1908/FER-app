3ce43f2529f6c05ce89a9b6da5507151f83f414d
cuognn <cuongacpe@gmail.com>
2023-05-09 14:38:01 +0700

    fix layouts and save

--- app_label.py
+++ app_label.py
@@ -14,7 +14,6 @@ class ImageLabeler:
         self.root = tk.Tk()
         self.root.title("Image Labeler")
         self.canvas = tk.Canvas(self.root, width=1080, height=600)
-        # self.canvas.pack()
         
         self.image_label = self.canvas.create_image(0, 0, anchor="nw")
         self.load_image()
@@ -31,50 +30,41 @@ class ImageLabeler:
         
         # selected_item function
         self.btn_add_cats = tk.Button(self.root, text='Add Selected', command=self.selected_item)
-        self.btn_add_cats.grid(column=1, row=1)
+        self.btn_add_cats.grid(column=2, row=0, sticky=tk.NW, padx=5, pady=180)
         
         # Placing the button and listbox
         # self.btn_add_cats.pack(side='bottom')
-        self.cat_box.grid(column=1, row=0, sticky=tk.W, padx=650, pady=5)
+        self.cat_box.grid(column=2, row=0, sticky=tk.NW, padx=5)
         # self.cat_box.pack()
 
         # Add gender radio buttons
         self.gender_var = tk.StringVar(value="male")
         self.gender_label = tk.Label(self.root, text="Gender:")
-        self.gender_label.grid(column=2,row=0, sticky=tk.NW, padx=5, pady=5)
-        # self.gender_label.pack()
-        self.male_radio = tk.Radiobutton(self.root, text="Male", variable=self.gender_var, value="male")
-        self.male_radio.grid(column=2,row=1, sticky=tk.NW, padx=5, pady=5)
-        # self.male_radio.pack()
-        self.male_radio = tk.Radiobutton(self.root, text="Female", variable=self.gender_var, value="female")
-        self.male_radio.grid(column=2,row=2, sticky=tk.NW, padx=5, pady=5)
-        # self.female_radio.pack()
+        self.gender_label.grid(column=2,row=0, sticky=tk.NW, padx=5, pady=220)
+        self.male_radio = tk.Radiobutton(self.root, text="Male", variable=self.gender_var, value="Male")
+        self.male_radio.grid(column=2,row=0, sticky=tk.NW, padx=5, pady=245)
+        self.male_radio = tk.Radiobutton(self.root, text="Female", variable=self.gender_var, value="Female")
+        self.male_radio.grid(column=2,row=0, sticky=tk.NW, padx=5, pady=270)
 
         # Add radio buttons for age
         self.age_var = tk.StringVar(value="kid")
         self.age_label = tk.Label(self.root, text="Age:")
-        self.age_label.grid(column=1,row=2, sticky=tk.S, padx=5, pady=5)
-        # self.age_label.pack()
+        self.age_label.grid(column=2,row=0, sticky=tk.NW, padx=5, pady=310)
 
-        self.age_kid = tk.Radiobutton(self.root, text="Kid", variable=self.age_var, value="kid")
-        self.age_label.grid(column=1,row=3, sticky=tk.NW, padx=5, pady=5)
-        # self.age_kid.pack()
+        self.age_kid = tk.Radiobutton(self.root, text="Kid", variable=self.age_var, value="Kid")
+        self.age_kid.grid(column=2,row=0, sticky=tk.NW, padx=5, pady=335)
 
-        self.age_teen = tk.Radiobutton(self.root, text="Teenager", variable=self.age_var, value="teen")
-        self.age_label.grid(column=1,row=4, sticky=tk.NW, padx=5, pady=5)
-        # self.age_teen.pack()
+        self.age_teen = tk.Radiobutton(self.root, text="Teenager", variable=self.age_var, value="Teenager")
+        self.age_teen.grid(column=2,row=0, sticky=tk.NW, padx=5, pady=360)
 
-        self.age_adult = tk.Radiobutton(self.root, text="Adult", variable=self.age_var, value="adult")
-        self.age_label.grid(column=1,row=5, sticky=tk.NW, padx=5, pady=5)
-        # self.age_adult.pack()
+        self.age_adult = tk.Radiobutton(self.root, text="Adult", variable=self.age_var, value="Adult")
+        self.age_adult.grid(column=2,row=0, sticky=tk.NW, padx=5, pady=385)
                
         self.next_button = tk.Button(self.root, text="Next", command=self.next_image)
-        self.next_button.grid(column=2,row=3, sticky=tk.NW, padx=5, pady=5)
-        # self.next_button.pack()
+        self.next_button.grid(column=2,row=0, sticky=tk.NW, padx=5, pady=410)
         
         self.save_button = tk.Button(self.root, text="Save", command=self.save_label)
-        self.save_button.grid(column=2,row=4, sticky=tk.NW, padx=5, pady=5)
-        # self.save_button.pack()
+        self.save_button.grid(column=2,row=0, sticky=tk.NW, padx=5, pady=435)
         
         self.root.mainloop()
 
@@ -90,32 +80,46 @@ class ImageLabeler:
         image = image.resize((600, 600))
         self.photo_image = ImageTk.PhotoImage(image)
         self.canvas.itemconfigure(self.image_label, image=self.photo_image)
-        self.canvas.grid(column=0, row=0, columnspan=2,rowspan=6, sticky=tk.W, padx=5, pady=5)
+        self.canvas.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.E, tk.W), columnspan=3, rowspan=2)
         
     def next_image(self):
         self.current_index += 1
         if self.current_index >= len(self.image_list):
             self.current_index = 0
         self.load_image()
-        
+           
     def save_label(self):
+
+        # Get categories
         
-        # After choose button save, label return a string
-        # import pdb; pdb.set_trace()
-        # label = self.label_var.get()
-        # label_categories = self.category_vars.get()
-        # label_gender = self.gender_var.get()
-        # label_age = self.age_var.get()
+        label_categories = '['
+        for idx,  cat in enumerate(self.categories):
+            if idx == 0:
+                label_categories = label_categories + f'{cat}'
+            else:
+                label_categories = label_categories + f', {cat}'
+        label_categories = label_categories + ']'
+        
+        gender = self.gender_var.get()
+        age = self.age_var.get()
+
+        df = pd.read_csv('asian.csv')
     
         image_path = os.path.join(self.image_folder, self.image_list[self.current_index])
-        base_name = image_path.split('/')
+        base_name = image_path.split('/')[-1]
         
-        # Compare base_name index in csv file
-        base_name = base_name.split('.')[0]
-        
-        # Write code for save label to csv file
-           
-            
+        # Find the row 
+        idx = df.index[df['Filename'] == base_name][0]
+
+        # Add the categories, gender, and age
+        df.at[idx, 'Categorical_Labels'] = label_categories
+        df.at[idx, 'Gender'] = gender
+        df.at[idx, 'Age'] = age
+
+        # Save to CSV file
+        df.to_csv('asian.csv', index=False)
+        print("Label Sucess!")  
+
 if __name__ == '__main__':
     print("Start: ")
     folder_context = '/home/cuongacpe/workspace/FER-app/data/context'
