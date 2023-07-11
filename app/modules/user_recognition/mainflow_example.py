4d89b62e1387d33b1d4ed969c0bdcca39c8bca58
cuognn <cuongacpe@gmail.com>
2023-07-11 16:11:53 +0700

    update verify user

--- app/modules/user_recognition/mainflow_example.py
+++ app/modules/user_recognition/mainflow_example.py
@@ -3,7 +3,37 @@ import cv2
 import os
 import base64
 import json
+import shutil
 from time import time
+
+
+models = [
+  "VGG-Face", 
+  "Facenet", 
+  "Facenet512", 
+  "OpenFace", 
+  "DeepFace", 
+  "DeepID", 
+  "ArcFace", 
+  "Dlib", 
+  "SFace",
+]
+
+backends = [
+  'opencv', 
+  'ssd', 
+  'dlib', 
+  'mtcnn', 
+  'retinaface', 
+  'mediapipe',
+  'yolov8',
+]
+
+distances = [
+    'cosine',
+    'euclidean',
+    'euclidean_l2'
+]
 
 num_frames = 5  # Number of frames to capture
 output_folder = "frames"  # Folder to save the frames
@@ -39,6 +69,8 @@ cv2.destroyAllWindows()
 results = []
 
 start = time()
+
+# analyze gender, race, age, emotion
 for frame_path in frames:
     result = DeepFace.analyze(frame_path, actions=['emotion', 'age', 'race', 'gender'], enforce_detection=False)
     results.append(result)
@@ -69,12 +101,48 @@ age = combined_result['age'] / len(results)
 gender = max(combined_result['gender'], key=combined_result['gender'].get)
 race = max(combined_result['race'], key=combined_result['race'].get)
 
+# identities person
+db_path = "/home/cuongacpe/Documents/AI-Chatbot-Synthesis/app/modules/user_recognition/user/database"
+file_name = f"representations_{models[1]}.pkl"
+file_name = file_name.replace("-", "_").lower()
+
+result = DeepFace.find(img_path = "/home/cuongacpe/Documents/AI-Chatbot-Synthesis/app/modules/user_recognition/tran-thanh.jpeg",
+      db_path = db_path, 
+      model_name = models[1],
+      detector_backend = backends[4],
+      distance_metric = distances[0]
+)
+if not result:
+    print("Person not found in the database")
+    os.remove(f"{db_path}/{file_name}")
+    name = input("What's your name: ")
+
+    # create new folder in dataset
+    new_folder_name = f"{name}"
+    new_folder_path = os.path.join(db_path, new_folder_name)
+    os.makedirs(new_folder_path, exist_ok=True)
+
+    # copy image from frames capture
+    existing_folder_path = '/home/cuongacpe/Documents/AI-Chatbot-Synthesis/app/modules/user_recognition/frames'
+    image_filenames = os.listdir(existing_folder_path)
+
+    for index, image_filename in enumerate(image_filenames):
+        source_path = os.path.join(existing_folder_path, image_filename)
+        new_image_filename = f"{new_folder_name}-{index}.jpg"
+        destination_path = os.path.join(new_folder_path, new_image_filename)
+        shutil.copy(source_path, destination_path)
+
+else:
+    name = result[0]['label'][0]
+    name = name.split("-")
+    name = name[0]
 
 flag = True
 
 if flag:
     
     default_details = {
+        "name": name,
         "emotion": emotion
     }
 
@@ -97,6 +165,7 @@ else:
             request_details['race'] = race
 
     # Include emotion details in the request details
+    request_details['name'] = name
     request_details['emotion'] = emotion
 
     if len(request_details) > 0:
